05cae23a3564bdea00d5ffd445d765f1b6fcf4d6
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-03 22:14:03 -0400

    add css to center text

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,7 @@
 export function Content() {
   return (
-    <div className="container">
-      <h1>Diane Code Diaries</h1>
+    <div className="container-fluid">
+      <h1 className="center-text">Diane Code Diaries</h1>
     </div>
   );
 }
