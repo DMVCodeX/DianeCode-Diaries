88f21be2d339ceea09bb92d5b0f482eb74918f4f
DIANELLY VENTURA <DMVCode@outlook.com>
2023-05-03 23:34:20 -0400

    add fonts

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,7 @@
 export function Content() {
   return (
     <div className="container-fluid">
-      <h1 className="center-text">Diane Code Diaries</h1>
+      <h1 className="center-text main-heading">Diane Code Diaries</h1>
       <img className="center-image" src="https://avatars.githubusercontent.com/u/122197490?v=4" alt="" />
     </div>
   );
